dcd7ed563d212e34b0204e9bd1edf5517c1d5954
Andrés Ignacio Torres <andresitorresm@gmail.com>
2021-11-18 00:40:06 -0400

    test: fix tests for main app

---
+++ test/featherapp_test.dart
@@ -0,0 +1,26 @@
+import 'package:flutter_test/flutter_test.dart';
+
+import 'package:feather/main.dart';
+import 'package:feather/screens/tabs.dart';
+import 'package:feather/screens/home.dart';
+
+void main() {
+  testWidgets('Main view has three tabs', (WidgetTester tester) async {
+    // Build our app and trigger a frame.
+    await tester.pumpWidget(const FeatherApp());
+
+    // Expect to find our Tabbed view
+    expect(find.byType(Tabs), findsOneWidget);
+
+    // Expect to find three tabs
+    expect(find.text('Home'), findsOneWidget);
+    expect(find.text('Local'), findsOneWidget);
+    expect(find.text('Fedi'), findsOneWidget);
+
+    // By default, the Home tab will be displayed
+    expect(find.byType(Home), findsOneWidget);
+    expect(find.text('Home coming soon!'), findsOneWidget);
+    expect(find.text('Local coming soon!'), findsNothing);
+    expect(find.text('Fedi coming soon!'), findsNothing);
+  });
+}
