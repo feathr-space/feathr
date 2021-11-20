e49b03a195d048e680bf7364f57262c623fe61b4
Andrés Ignacio Torres <andresitorresm@gmail.com>
2021-11-20 02:46:29 -0400

    feat: load env vars on app startup

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,12 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
 
 import 'package:feathr/app.dart';
 
-void main() {
+void main() async {
+  // Loading environment variables from file
+  await dotenv.load(fileName: ".env");
+
+  // Launching the app!
   runApp(const FeathrApp());
 }
