0f72f25c9b256aba7b6a9d776c679a274efb7d5f
Andrés Ignacio Torres <andresitorresm@gmail.com>
2021-12-02 23:16:01 -0400

    feat: add api connection to request timeline statuses

--- lib/services/api.dart
+++ lib/services/api.dart
@@ -4,6 +4,7 @@ import 'package:http/http.dart' as http;
 import 'package:oauth2_client/oauth2_helper.dart';
 
 import 'package:feathr/helpers/auth.dart';
+import 'package:feathr/data/status.dart';
 import 'package:feathr/data/account.dart';
 
 /// Custom exception to be thrown by the API service for unhandled cases.
@@ -13,6 +14,10 @@ class ApiException implements Exception {
 
   ApiException(this.message);
 }
+
+/// Each [TimelineType] represents a specific set of restrictions for querying
+/// a Mastodon timeline.
+enum TimelineType { home, local, fedi }
 
 class ApiService {
   /// URL of the Mastodon instance to perform auth with
@@ -24,6 +29,55 @@ class ApiService {
 
   /// [Account] instance of the current logged-in user.
   Account? currentAccount;
+
+  /// Given a timeline type, an optional maxId and a limit of statuses,
+  /// requests the `limit` amount of statuses from the selected timeline
+  /// (according to the `timelineType`), using `maxId` as an optional
+  /// starting point.
+  Future<List<Status>> getStatusList(
+    TimelineType timelineType,
+    String? maxId,
+    int limit,
+  ) async {
+    // Depending on the type, we select and restrict the api URL to use,
+    // limiting the amount of posts we're requesting according to `limit`
+    String apiUrl;
+
+    if (timelineType == TimelineType.home) {
+      apiUrl = "$instanceUrl/api/v1/timelines/home?limit=$limit";
+    } else {
+      // Both the Local and the Fedi timelines use the same base endpoint
+      apiUrl = "$instanceUrl/api/v1/timelines/public?limit=$limit";
+
+      if (timelineType == TimelineType.local) {
+        apiUrl += "?local=true";
+      }
+    }
+
+    // If `maxId` is not null, we'll use it as a threshold so that we only
+    // get posts older (further down in the timeline) than this one
+    // TODO: request both ways, not only older toots
+    if (maxId != null) {
+      apiUrl += "?max_id=$maxId";
+    }
+
+    http.Response resp = await helper.get(apiUrl);
+
+    if (resp.statusCode == 200) {
+      // The response is a list of json objects
+      List<dynamic> jsonDataList = jsonDecode(resp.body);
+
+      return jsonDataList
+          .map(
+            (statusData) => Status.fromJson(statusData as Map<String, dynamic>),
+          )
+          .toList();
+    }
+
+    throw ApiException(
+      "Unexpected status code ${resp.statusCode} on `getStatusList`",
+    );
+  }
 
   /// Returns the current account as cached in the instance,
   /// retrieving the account details from the API first if needed.
@@ -49,7 +103,8 @@ class ApiService {
     }
 
     throw ApiException(
-        "Unexpected status code ${resp.statusCode} on `getAccount`");
+      "Unexpected status code ${resp.statusCode} on `getAccount`",
+    );
   }
 
   /// Performs an authenticated query to the API in order to force the log-in
