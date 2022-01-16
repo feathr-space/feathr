e2587cfbb6060bee47353bdc29dd215c8561f1a4
Andrés Ignacio Torres <andresitorresm@gmail.com>
2022-01-15 21:56:08 -0400

    test: add visual tests for status card

---
+++ test/widgets_test/status_card_test.dart
@@ -0,0 +1,59 @@
+import 'package:feathr/data/status.dart';
+import 'package:feathr/services/api.dart';
+import 'package:flutter_feather_icons/flutter_feather_icons.dart';
+import 'package:flutter_test/flutter_test.dart';
+
+import 'package:flutter/material.dart';
+import 'package:feathr/widgets/status_card.dart';
+
+import '../utils.dart';
+
+void main() {
+  testWidgets(
+    'Status Card is rendered properly',
+    (WidgetTester tester) async {
+      ApiService apiService = getTestApiService();
+      Status status = Status(
+        id: "12345678",
+        content: "<p>This is a toot!</p>",
+        account: apiService.currentAccount!,
+        favorited: true,
+        reblogged: false,
+        bookmarked: true,
+      );
+
+      await tester.pumpWidget(
+        MaterialApp(
+          home: Scaffold(
+            body: Column(
+              children: [
+                StatusCard(
+                  status,
+                  apiService: apiService,
+                ),
+              ],
+            ),
+          ),
+        ),
+      );
+
+      // Initial render
+      expect(find.byType(StatusCard), findsOneWidget);
+      expect(find.text('display name'), findsOneWidget);
+      expect(find.text('username'), findsOneWidget);
+      expect(find.text('This is a toot!'), findsOneWidget);
+      expect(
+        find.widgetWithIcon(IconButton, FeatherIcons.bookmark),
+        findsOneWidget,
+      );
+      expect(
+        find.widgetWithIcon(IconButton, FeatherIcons.star),
+        findsOneWidget,
+      );
+      expect(
+        find.widgetWithIcon(IconButton, FeatherIcons.repeat),
+        findsOneWidget,
+      );
+    },
+  );
+}
