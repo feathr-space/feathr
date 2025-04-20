d44f41f054b8a82329ab587837ffa638a006b33e
Andrés Ignacio Torres <dev@aitorres.com>
2025-04-20 11:29:14 -0700

    test: increase test coverage for existing screens

---
+++ test/screens_test/login_test.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_test/flutter_test.dart';
+
+import 'package:feathr/screens/login.dart';
+
+import '../utils.dart';
+
+void main() {
+  testWidgets('Login screen is rendered correctly',
+      (WidgetTester tester) async {
+    final apiService = getTestApiService();
+
+    await tester.pumpWidget(MaterialApp(
+      home: Login(
+        apiService: apiService,
+      ),
+    ));
+
+    // Expect to find the app's name
+    expect(find.text('feathr'), findsOneWidget);
+  });
+}
---
+++ test/screens_test/user_test.dart
@@ -0,0 +1,31 @@
+import 'package:feathr/widgets/timeline.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_test/flutter_test.dart';
+
+import 'package:feathr/screens/user.dart';
+
+import '../utils.dart';
+
+void main() {
+  testWidgets('User screen displays user information',
+      (WidgetTester tester) async {
+    final apiService = getTestApiService();
+    final account = apiService.currentAccount!;
+
+    await tester.pumpWidget(MaterialApp(
+      home: User(
+        account: account,
+        apiService: apiService,
+      ),
+    ));
+
+    // Expect to find the user's name
+    expect(find.text(account.displayName), findsOneWidget);
+
+    // Expect to find the user's acct
+    expect(find.text(account.acct), findsOneWidget);
+
+    // Expect to find a Timeline widget
+    expect(find.byType(Timeline), findsOneWidget);
+  });
+}
