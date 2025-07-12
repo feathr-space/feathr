c44f29bc053c068be2ba1c5f8acff50f7b3d97ff
Andrés Ignacio Torres <dev@aitorres.com>
2025-07-11 20:25:27 -0700

    test: increase coverage to 75% (closes #15)

--- test/app_test.dart
+++ test/app_test.dart
@@ -1,7 +1,10 @@
 import 'package:flutter_test/flutter_test.dart';
 
+import 'package:flutter/material.dart';
+
 import 'package:feathr/app.dart';
 import 'package:feathr/screens/login.dart';
+import 'package:feathr/screens/about.dart';
 import 'package:feathr/screens/timeline_tabs.dart';
 import 'package:feathr/widgets/title.dart';
 
@@ -17,5 +20,18 @@ void main() {
     // Expect to find the title of the app
     expect(find.byType(TitleWidget), findsOneWidget);
     expect(find.text('feathr'), findsOneWidget);
+  });
+
+  testWidgets('Routes render correct widgets', (WidgetTester tester) async {
+    await tester.pumpWidget(FeathrApp());
+
+    // Test /login route (initial route)
+    expect(find.byType(Login), findsOneWidget);
+
+    NavigatorState navigator = tester.state(find.byType(Navigator));
+
+    navigator.pushNamed('/about');
+    await tester.pumpAndSettle();
+    expect(find.byType(About), findsOneWidget);
   });
 }
